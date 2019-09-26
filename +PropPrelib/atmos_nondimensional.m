9cc9995d0fc89ea04f04765ddb93eb8e8bc99d05
Michael <captain@mikeddavidson.com>
2019-09-26 19:17:28 -0400

    Fixed bug in atmos_nondimensional

--- +PropPrelib/atmos_nondimensional.m
+++ +PropPrelib/atmos_nondimensional.m
@@ -1,7 +1,7 @@
 function [theta, delta, sigma] = atmos_nondimensional(h)
 import PropPrelib.*;
 
-[Tstd, ~, Pstd, rhostd] = atmos(0);
+[Tstd, ~, Pstd, rhostd] = atmos(0, AtmosModel.Standard);
 [T, ~, P, rho] = atmos(h);
 theta = T/Tstd;
 delta = P/Pstd;
