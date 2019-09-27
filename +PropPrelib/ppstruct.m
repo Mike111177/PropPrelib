2cf53ff16bd709466051cdc5b9125cdf7c67e2f8
Michael <captain@mikeddavidson.com>
2019-09-27 02:53:11 -0400

    Improved stat printing

--- +PropPrelib/ppstruct.m
+++ +PropPrelib/ppstruct.m
@@ -1,15 +1,25 @@
 function ppstruct(s, indent)
-    if nargin == 1
+    if nargin < 2
         indent = 0;
     end
     names = fieldnames(s);
     labels = pad(string(names), 'left');
-    istr = blanks(indent);
+    
+    lines = cell(1,numel(names));
     for k = 1:numel(names)
         value = s.(names{k});
         if isnumeric(value) && isscalar(value)
-            fprintf([istr '%s: %.4g\n'], labels{k}, value);
+            lines{k} = sprintf('%s: %.4g', labels{k}, value);
         end
+    end
+    istr = blanks(indent);
+    sepstr = blanks(5);
+    if mod(numel(lines),2) == 1
+        lines{end+1} = "";
+    end
+    rows = join(reshape(pad(string(lines), 'right'), [], 2), sepstr);
+    for k = 1:numel(rows)
+        fprintf([istr rows{k} '\n'])
     end
 end
 
